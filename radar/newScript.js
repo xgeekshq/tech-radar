ea7216e5b89967927b12dfa1f79f51702d88c840
Mario Costa <mariocozta@gmail.com>
2021-09-10 10:30:56 +0100

    Added new css styleSheet for hiding standard buttons/added hide func for maximized radar

--- radar/newScript.js
+++ radar/newScript.js
@@ -1,3 +1,32 @@
-'use strict'
 
-const quadrantOne = document.querySelector();
+const radar = document.querySelector(".radarr");
+const sideMenu = document.querySelector(".techs");
+
+function disableStyleSheet() {
+    setTimeout(function () {
+        document.styleSheets[4].disabled = true;
+    }, 500);
+}
+
+radar.addEventListener('click',function(e){
+
+    //console.log(document.styleSheets);
+
+    if(e.path[0].id !== "radar-plot" && JSON.stringify(e.target.className) === "{}"){
+        console.log("map maximazed");
+        sideMenu.style.opacity="0";
+        disableStyleSheet();
+
+    }else if( JSON.stringify(e.target.className) === "\"home-link selected\""){
+        console.log("Map minimized");
+        //sideMenu.style.display ="initial";
+        sideMenu.style.opacity="1";
+        document.styleSheets[4].disabled = false;
+
+    }
+    
+    
+
+});
+
+
