fea8050068996b459f92733bd6f3f77e36ecda39
Mario Costa <mariocozta@gmail.com>
2021-09-10 15:06:18 +0100

    Added open tech modal func to side menu items

--- radar/newScript.js
+++ radar/newScript.js
@@ -2,7 +2,7 @@
 const radar = document.querySelector(".radarr");
 const sideMenu = document.querySelector(".techs");
 
-function disableStyleSheet() {
+function disableSideMenuStyleSheet() {
     setTimeout(function () {
         document.styleSheets[4].disabled = true;
     }, 500);
@@ -13,20 +13,13 @@ radar.addEventListener('click',function(e){
     //console.log(document.styleSheets);
 
     if(e.path[0].id !== "radar-plot" && JSON.stringify(e.target.className) === "{}"){
-        console.log("map maximazed");
         sideMenu.style.opacity="0";
-        disableStyleSheet();
+        disableSideMenuStyleSheet();
 
     }else if( JSON.stringify(e.target.className) === "\"home-link selected\""){
-        console.log("Map minimized");
-        //sideMenu.style.display ="initial";
         sideMenu.style.opacity="1";
         document.styleSheets[4].disabled = false;
 
     }
-    
-    
-
 });
-
 
